5c5f13a8134ada037d294ebb72be0d431f2fe6d2
DedanKIarie <dedanbroo@gmail.com>
2025-03-25 10:25:19 +0300

    Created the log in feature to store player's data

--- index.js
+++ index.js
@@ -10,3 +10,51 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`JSON Server is running on port ${PORT}`);
 });
+
+//start function
+function getplayer() {
+    let playerbox = document.getElementById("username").value.trim();
+    if (playerbox === "") {
+        console.log("Username is empty");
+        return;
+    }
+
+    fetch("https://guess-the-country-ofgg.onrender.com/players")
+        .then(response => response.json())
+        .then(players => {
+            let playerExists = false;
+
+            for (let i = 0; i < players.length; i++) {
+                if (players[i].username === playerbox) {
+                    playerExists = true;
+                    console.log("Player exists:", players[i]);
+                    break;
+                }
+            }
+
+            if (!playerExists) {
+                console.log(`player not found adding ${playerbox}`);
+                
+                let newPlayer = { username: playerbox, score: 0, Wins:0 };
+
+                fetch("https://guess-the-country-ofgg.onrender.com/players", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(newPlayer)
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log("New player added:", data);
+                });
+            }
+        })
+}
+
+function login() {
+    getplayer();
+}
+
+
+function getcountries() {
+    
+}
